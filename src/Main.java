fdf3069ae56dd40812cd69563acee12785acd2f6
brady feldhaus <feldhabl@mail.uc.edu>
2022-02-09 14:11:34 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	    int numOne = 3;
+        int numTwo = 6;
+
+        if (numOne == numTwo)
+        {
+            System.out.println("The two numbers are equal");
+        }
+        else if (numOne < numTwo)
+        {
+            System.out.println(numOne + " is less than " + numTwo);
+        }
+        else
+        {
+            System.out.println(numTwo + "is less than " + numOne);
+        }
+    }
+}
